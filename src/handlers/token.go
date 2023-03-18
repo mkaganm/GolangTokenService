4913541478ca17be39e27a20901e8c82ad25cb2c
kagan.meric <kagan.meric@enuygun.com>
2023-03-18 17:47:09 +0300

    feat :: add token.go

---
+++ src/handlers/token.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"GolangJWTService/src/auth"
+	"GolangJWTService/src/errors"
+	"GolangJWTService/src/models"
+	"encoding/json"
+	"github.com/golang-jwt/jwt/v4"
+	"log"
+	"net/http"
+	"time"
+)
+
+var SECRET = []byte("super-secret-auth-key")
+
+func GetToken(w http.ResponseWriter, r *http.Request) {
+
+	NotPost(w, r)
+	SetContentJson(w)
+
+	if r.Header["X-Api-Key"] != nil {
+
+		if auth.CheckPassword(r.Header["X-Api-Key"][0], models.ApiKey{}.GetXApiKey().XApiKey) {
+
+			token, err := CreateToken()
+			errors.CheckErr(err)
+
+			log.Default().Print("success token")
+
+			m := make(map[string]string)
+			m["token"] = token
+			m["status"] = "success"
+
+			jsonS, _ := json.Marshal(m)
+
+			_, err = w.Write(jsonS)
+			errors.CheckErr(err)
+
+		} else {
+			SendUnAuthWrite(w)
+		}
+	} else {
+		SendUnAuthWrite(w)
+	}
+}
+
+func CreateToken() (string, error) {
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["exp"] = time.Now().Add(time.Hour).Unix()
+
+	tokenStr, err := token.SignedString(SECRET)
+	errors.CheckErr(err)
+
+	return tokenStr, nil
+}
