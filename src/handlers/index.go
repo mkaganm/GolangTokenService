e2c288b3a0ac68fe3fa2f58e0e519597a2d2108d
kagan.meric <kagan.meric@enuygun.com>
2023-03-18 17:46:58 +0300

    feat :: add index.go

---
+++ src/handlers/index.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"GolangJWTService/src/errors"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	NotPost(w, r)
+
+	SetContentJson(w)
+
+	log.Default().Print("success access home page")
+
+	m := make(map[string]string)
+	m["status"] = "success"
+	m["message"] = "home page"
+
+	jsonM, _ := json.Marshal(m)
+
+	var _, err = w.Write(jsonM)
+
+	errors.CheckErr(err)
+}
