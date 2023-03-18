0ecc50c2349ef60fba2ca131687c871c23e46887
kagan.meric <kagan.meric@enuygun.com>
2023-03-18 17:47:28 +0300

    feat :: update main.go

--- src/main.go
+++ src/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"GolangJWTService/src/errors"
+	"GolangJWTService/src/handlers"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := http.NewServeMux()
+	r.HandleFunc("/token", handlers.GetToken)
+	r.Handle("/", handlers.ValidateToken(handlers.Index))
+
+	log.Default().Print("Server localhost:8081 is started")
+
+	defer func(addr string, handler http.Handler) {
+		err := http.ListenAndServe(addr, handler)
+		errors.CheckErr(err)
+	}(":8081", r)
 }
